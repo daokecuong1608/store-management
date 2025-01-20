a1ebfa24b4026eb84baa3d24f625fc94de160304
hatruc <118835705+hatruc@users.noreply.github.com>
2025-01-20 15:32:12 +0700

    truc 15h32

--- src/main/java/com/sapo/store_management/model/Order.java
+++ src/main/java/com/sapo/store_management/model/Order.java
@@ -10,16 +10,16 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+//import lombok.Getter;
+//import lombok.Setter;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Table(name = "`order`")
-@Getter
-@Setter
+//@Getter
+//@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Order {
@@ -72,5 +72,120 @@ public class Order {
     @PreUpdate
     public void handleBeforeUpdate() {
         this.updated_at = Instant.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public int getCoupon_id() {
+        return coupon_id;
+    }
+
+    public void setCoupon_id(int coupon_id) {
+        this.coupon_id = coupon_id;
+    }
+
+    public int getStaff_id() {
+        return staff_id;
+    }
+
+    public void setStaff_id(int staff_id) {
+        this.staff_id = staff_id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    @Min(value = 0, message = "Total phải lớn hơn hoặc bằng 0")
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(@Min(value = 0, message = "Total phải lớn hơn hoặc bằng 0") int total) {
+        this.total = total;
+    }
+
+    @Min(value = 0, message = "Payment phải lớn hơn hoặc bằng 0")
+    public int getPayment() {
+        return payment;
+    }
+
+    public void setPayment(@Min(value = 0, message = "Payment phải lớn hơn hoặc bằng 0") int payment) {
+        this.payment = payment;
+    }
+
+    @Min(value = 0, message = "Change phải lớn hơn hoặc bằng 0")
+    public int getChange_given() {
+        return change_given;
+    }
+
+    public void setChange_given(@Min(value = 0, message = "Change phải lớn hơn hoặc bằng 0") int change_given) {
+        this.change_given = change_given;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public Instant getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Instant created_at) {
+        this.created_at = created_at;
+    }
+
+    public Instant getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Instant updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public List<OrderProduct> getOrderProducts() {
+        return orderProducts;
+    }
+
+    public void setOrderProducts(List<OrderProduct> orderProducts) {
+        this.orderProducts = orderProducts;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
     }
 }
